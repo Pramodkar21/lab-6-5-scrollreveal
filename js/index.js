6310771ba9e6980af0a681b6ed87ab9c4b23c15d
Pramodkar21 <karthi21@msu.edu>
2026-02-22 15:21:02 +0000

    updated js

--- js/index.js
+++ js/index.js
@@ -2,9 +2,8 @@ window.sr = ScrollReveal({ reset: false });
 
 sr.reveal('.reveal', {
     duration: 800,
-    distance: '20px',
+    distance: '40px',
     opacity: 0,
-    delay: 200
+    delay: 200,
+    viewOffset: { top: 300, bottom: 0 }
 });
-
-
